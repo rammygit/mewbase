d8da7b7114e99ab37c559f6bedb550266b602c1c
Tim Fox <tim.fox@uk.tesco.com>
2016-10-01 14:57:02 +0100

    something on example

--- src/example/java/com/tesco/mewbase/shopping/Example.java
+++
@@ -1,7 +0,0 @@
-package com.tesco.mewbase.shopping;
-
-/**
- * Created by tim on 30/09/16.
- */
-public class Example {
-}
---
+++ src/example/java/com/tesco/mewbase/shopping/ExampleServer.java
@@ -0,0 +1,27 @@
+package com.tesco.mewbase.shopping;
+
+import com.tesco.mewbase.common.SubDescriptor;
+import com.tesco.mewbase.server.Server;
+import com.tesco.mewbase.server.ServerOptions;
+import com.tesco.mewbase.server.impl.ServerImpl;
+
+/**
+ *
+ *
+ *
+ * Created by tim on 30/09/16.
+ */
+public class ExampleServer {
+
+    public static void main(String[] args) {
+
+        Server mewbase = new ServerImpl(new ServerOptions());
+        mewbase.installFunction("myfunc1", new SubDescriptor().setStreamName("com.tesco.foo"), (ctx, re) -> {
+            //do something
+        });
+        mewbase.installFunction("myfunc1", new SubDescriptor().setStreamName("com.tesco.bar"), (ctx, re) -> {
+            //do something
+        });
+        mewbase.start();
+    }
+}
