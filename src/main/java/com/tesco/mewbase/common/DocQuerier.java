ebc1a36d58fd1ae355668b8e7f9af3e711809cd8
Andy Hedges <andy@hedges.net>
2016-11-17 19:28:55 +0000

    Committing intial javadoc comments

--- src/main/java/com/tesco/mewbase/common/DocQuerier.java
+++ src/main/java/com/tesco/mewbase/common/DocQuerier.java
@@ -6,14 +6,31 @@ import com.tesco.mewbase.client.QueryResult;
 import java.util.concurrent.CompletableFuture;
 
 /**
+ * Used for finding documents within a given binder
+ *
  * Created by tim on 23/09/16.
  */
 public interface DocQuerier {
 
     // TODO use RxJava for this too
 
+  /**
+   * Retrieve a document by its binder name and its identifier within that binder
+   *
+   * @param binderName the name of the binder
+   * @param id the name of the document within the binder
+   * @return
+   */
     CompletableFuture<BsonObject> findByID(String binderName, String id);
 
-    CompletableFuture<QueryResult> findMatching(String binderName, BsonObject matcher);
+  /**
+   * Find matching documents within a given binder
+   * TODO: work out the structure of a matcher
+   * @param binderName the name of the binder to run the matcher against
+   * @param matcher
+   * @return
+   */
+
+  CompletableFuture<QueryResult> findMatching(String binderName, BsonObject matcher);
 
 }
--- src/main/java/com/tesco/mewbase/doc/DocManager.java
+++ src/main/java/com/tesco/mewbase/doc/DocManager.java
@@ -6,11 +6,20 @@ import com.tesco.mewbase.common.DocQuerier;
 import java.util.concurrent.CompletableFuture;
 
 /**
+ *
  * Created by tim on 30/09/16.
  */
 public interface DocManager extends DocQuerier {
 
+    //TODO: should probable be public static final
     String ID_FIELD = "id";
 
+  /**
+   * Save a named document to a binder with the given name
+   * @param binder name of binder to save the document to
+   * @param id the name of the document within the binder
+   * @param doc the document to save
+   * @return
+   */
     CompletableFuture<Void> save(String binder, String id, BsonObject doc);
 }
